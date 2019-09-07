623895f55cc4128b0ad886b8a54c7d9cf75ab3f9
Raiana <silvaraiana.rs@gmail.com>
2019-09-07 13:44:17 -0300

    o formulario do cadastro ai

---
+++ formulariocadastro.php
@@ -0,0 +1,23 @@
+<div class="card">
+
+    <div class="card-body">
+
+        <h2 class="card-title m-2">Cadastro</h2>
+
+
+        <form method="post" action="login.php" class="m-3 p-3">
+            Nome<br>
+            <input type="email" class="form-control" placeholder="digite seu e-mail" name="email"><br>
+            E-mail<br>
+            <input type="email" class="form-control" placeholder="digite seu e-mail" name="email"><br>
+            Senha<br>
+            <input type="password" class="form-control" placeholder="digite sua senha" name="senha"><br>           
+            Confirme a senha<br>
+            <input type="password" class ="form-control" placeholder="digite sua senha novamente" name="confsenha"><br>
+            <br><br>
+            <hr>
+            <input type="submit" class="btn btn-primary btn" value="Salvar">
+        </form>
+    </div>
+
+</div>
