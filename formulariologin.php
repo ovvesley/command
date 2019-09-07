5389d5db0c5cb2fdd71e2a5aea0f06f16de7815a
Raiana <silvaraiana.rs@gmail.com>
2019-09-07 11:45:43 -0300

    formulario de login ai

---
+++ formulariologin.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <title>Login</title>
+
+    </head>
+    <body>
+        <div >
+            <nav>
+            </nav>
+            <section>
+                <h1>Login de Usuário</h1>
+                <hr><br><br>
+
+                <form method="post" action="login.php">
+
+                    E-mail<br>
+                    <input type="email" placeholder="digite seu e-mail" name="email" ><br>
+                    Senha<br>
+                    <input type="password" placeholder="digite sua senha" name="senha"><br> 
+                     <br><br>
+                     <input type="submit" value="Salvar" class="btn">
+                   
+                </form>
+            </section>
+        </div>
+    </body>
+</html>
