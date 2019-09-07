cfe94187be0257fd3c629c3cfc1d47a54b05dbff
Raiana <silvaraiana.rs@gmail.com>
2019-09-07 16:28:08 -0300

    formulário das publicações ai

---
+++ formulariopublicacao.php
@@ -0,0 +1,17 @@
+<div >
+
+    <div >
+
+        
+        <form method="post" action="novapublicacao.php">
+            Título: <br>
+            <input type="text" class="form-control" name="titulo"><br>
+            Mensagem: <br>
+            <textarea name="mensagem" class="form-control" rows="5" placeholder="Insira seu(s) comando(s)" size=60></textarea>
+            <br><br>
+            <hr>
+           <input  type="submit"  value="Salvar">
+        </form>
+    </div>
+
+</div>
