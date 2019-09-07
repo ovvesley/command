9b212362342d2d3e6cd8b07e02b50cc0af0068c1
Raiana <silvaraiana.rs@gmail.com>
2019-09-07 14:19:15 -0300

    index atualizado

--- index.php
+++ index.php
@@ -15,31 +15,13 @@
 </head>
 
 <body>
-
-
-
     <div class="container login-container">
         <div class="row">
             <div class="col-md-6 login-form-1">
                 <?php require("./formulariologin.php") ?>
             </div>
             <div class="col-md-6 login-form-2">
-                <h3>Cadastro</h3>
-                <form>
-                    <div class="form-group">
-                        <input type="text" class="form-control" placeholder="Your Email *" value="" />
-                    </div>
-                    <div class="form-group">
-                        <input type="password" class="form-control" placeholder="Your Password *" value="" />
-                    </div>
-                    <div class="form-group">
-                        <input type="submit" class="btnSubmit" value="Login" />
-                    </div>
-                    <div class="form-group">
-
-                        <a href="#" class="ForgetPwd" value="Login">Forget Password?</a>
-                    </div>
-                </form>
+            <?php require ("./formulariocadastro.php")  ?>
             </div>
         </div>
     </div>
