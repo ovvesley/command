6632c800a475300bbaabb20435d19654a9cb34c9
Raiana <silvaraiana.rs@gmail.com>
2019-09-07 14:28:12 -0300

    ajustando erro no formulario de cadastro

--- formulariocadastro.php
+++ formulariocadastro.php
@@ -5,7 +5,7 @@
         <h2 class="card-title m-2">Cadastro</h2>
 
 
-        <form method="post" action="login.php" class="m-3 p-3">
+        <form method="post" action="cadastro.php" class="m-3 p-3">
             Nome<br>
             <input type="name" class="form-control" placeholder="digite seu e-mail" name="email"><br>
             E-mail<br>
